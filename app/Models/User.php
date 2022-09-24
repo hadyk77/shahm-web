71cc6406c20e1cea6a7c130b540522869e22e168
Abdalrahman Almesery <Abdelrahman.pme18@suezuni.edu.eg>
2022-09-24 18:24:46 +0200

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -100,8 +100,7 @@ class User extends Authenticatable implements HasMedia
     ];
 
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        "social_login_id" => "integer"
+        'email_verified_at' => 'datetime'
     ];
 
     public function registerMediaCollections(): void
