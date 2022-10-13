63dbc44cef7f4e4a8613f1ddc72f30e1ce8257eb
Abdalrahman Almesery <Abdelrahman.pme18@suezuni.edu.eg>
2022-10-13 08:25:31 +0200

    WIP

--- app/Http/Resources/Rate/RateResource.php
+++ app/Http/Resources/Rate/RateResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources\Rate;
 
+use App\Models\Captain;
+use App\Models\Service;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Str;
@@ -14,9 +17,28 @@ class RateResource extends JsonResource
      */
     public function toArray($request)
     {
+        $ratable = [];
+
+        if ($this->model_type == Service::class) {
+            $service = Service::query()->find($this->model_id);
+            $ratable = [
+                "id" => $service->id,
+                "title" => $service->title,
+                "image" => $service->icon
+            ];
+        }
+        if ($this->model_type == User::class || $this->model_type == Captain::class) {
+            $user = User::query()->find($this->model_id);
+            $ratable = [
+                "id" => $user->id,
+                "name" => $user->name,
+                "profile_image" => $user->profile_image,
+            ];
+        }
         return [
             "id" => $this->id,
             "rate_for" => strtolower(Str::remove("App\\Models\\",  $this->model_type)),
+            "ratable_type" => $ratable,
             "rate" => $this->rate,
             "text" => $this->text,
         ];
