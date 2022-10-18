cb75b0a2b13001e5b53a182b8055c008f990d4aa
Abdalrahman Almesery <Abdelrahman.pme18@suezuni.edu.eg>
2022-10-18 21:56:20 +0200

    WIP

--- realtime/chat.js
+++ realtime/chat.js
@@ -44,33 +44,10 @@ io.on('connection', (socket) => {
 
     logger.info(`Socket with id = ${socket.id}`);
 
-    socket.emit("message", "Welcome to socket io from Aqary App");
+    socket.emit("message", "Welcome to socket io from shahm app");
 
     socket.on("message", (data) => {
         logger.info(data);
-    });
-
-    socket.on("addUserToChatRoom", ({chat_uuid}) => {
-        socket.join(chat_uuid);
-        logger.info("chat_uuid from room => " + chat_uuid)
-    });
-
-    socket.on("sendChatMessage", async ({token, chat_uuid, chat_id, message_id, message_from}) => {
-        logger.info("token => " + token)
-        logger.info("chat_uuid => " + chat_uuid)
-        logger.info("message_id => " + message_id);
-        logger.info("message_from => " + message_from);
-        let url = `${process.env.BASE_URL}/chat-message/${chat_uuid}/message/${message_id}`;
-        axios
-            .get(url, {headers: {"Authorization": `Bearer ${token}`}})
-            .then(response => {
-                socket
-                    .to(chat_uuid)
-                    .emit("messageResponseForChat", response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
     });
 
 });
